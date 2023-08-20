b610f8271b6fb39f2c40a9bc1b1d124a2616d3a6
Dery Rhamdani <deryktrc001@gmail.com>
2023-08-20 15:32:24 +0700

    test

---
+++ resources/views/home.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FAQ | KRIS</title>
+
+    <!-- Favicon -->
+    <link href="img/favicon.png" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&family=Roboto:wght@500;700&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<style>
+    header {
+        background-color: #1C2035;
+        color: #fff;
+        text-align: center;
+        padding: 20px;
+        margin-bottom: 20px;
+    }
+
+    footer {
+        background-color: white;
+        color: #fff;
+        text-align: center;
+        padding: 10px 0;
+        margin-top: 20px;
+    }
+</style>
+
+<body>
+    <div class="container-xxl   ">
+        <div class="container">
+            <header>
+                <h1 class="text-light">FAQ Page</h1>
+                <p>KRIS Module Reservation</p>
+            </header>
+            <div class="accordion-block wow fadeInUp" data-wow-delay="0.3 ">
+                <div class="accordion row" id="accordionPanelsStayOpenExample">
+                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                        <div class="accordion-item mb-3">
+                            <h2 class="accordion-header" id="panelsStayOpen-headingOne">
+                                <button class="accordion-button text-black fs-7 text-dark" type=" button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
+                                    Apa perbedaan Individual, Corporate, Educational dll pada field customer grup ?
+                                </button>
+                            </h2>
+                            <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
+                                <p></p>
+                                <div class="accordion-body fs-7 fw-500 pt-0">
+                                    - Customer Individual adalah
+                                    <br>
+                                    - Customer Corporate adalah
+                                    <br>
+                                    - Customer Educational Institution adalah
+                                    <br>
+                                    - Customer Government Agencies adalah
+                                    <br>
+                                    - Customer Non-Governmental Organization adalah
+                                    <br>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item mb-3">
+                            <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
+                                <button class="accordion-button collapsed text-black fs-7 text-dark" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
+                                    Bagaimana jika ingin menambahkan sales person ?
+                                </button>
+                            </h2>
+                            <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
+                                <p></p>
+                                <div class="accordion-body fs-7 fw-500 pt-0">
+                                    Untuk menambahkan Sales Person bisa request by Grup WhatsApp [MNR] RESERVATION
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item mb-3">
+                            <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                                <button class="accordion-button collapsed text-black fs-7 text-dark" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                                    Bagaimana jika salah input payment date or event date ?
+                                </button>
+                            </h2>
+                            <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+                                <p></p>
+                                <div class="accordion-body fs-7 fw-500 pt-0">
+                                    Jika salah input payment date or event date Quotation/Order bisa di cancel dan wajib mengisi reason.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item mb-3">
+                            <h2 class="accordion-header" id="panelsStayOpen-headingThree">
+                                <button class="accordion-button collapsed text-black fs-7 text-dark" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
+                                    Jika quotation sudah di post to order apakah bisa di edit ?
+                                </button>
+                            </h2>
+                            <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
+                                <p></p>
+                                <div class="accordion-body fs-7 fw-500 pt-0">
+                                    Jika quotation sudah sudah menjadi order maka quotation tidak bisa di edit, jadi pastikan kembali sebelum post to order quotation sudah sesuai .
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- row -->
+                    <div class="col-xxl-6 col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
+                        <div class="accordion-item mb-3">
+                            <h2 class="accordion-header" id="panelsStayOpen-headingfour">
+                                <button class="accordion-button collapsed text-black fs-7 text-dark" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapsefour" aria-expanded="false" aria-controls="panelsStayOpen-collapsefour">
+                                    Bagaimana jika ingin menambahkan product/items atau merubah harga, disc dll ?
+                                </button>
+                            </h2>
+                            <div id="panelsStayOpen-collapsefour" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingfour">
+                                <p></p>
+                                <div class="accordion-body fs-7 fw-500 pt-0">
+                                    Untuk mengubah harga, discount, product dan open price product bisa di ajukan oleh team finance unit masing-masing dan menghubungi team Finance Holding.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item mb-3">
+                            <h2 class="accordion-header" id="panelsStayOpen-headingfive">
+                                <button class="accordion-button collapsed text-black fs-7 text-dark" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapsefive" aria-expanded="false" aria-controls="panelsStayOpen-collapsefive">
+                                    Bagaimana jika saat membuat quotation tapi tidak perlu location ?
+                                </button>
+                            </h2>
+                            <div id="panelsStayOpen-collapsefive" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingfour">
+                                <p></p>
+                                <div class="accordion-body fs-7 fw-500 pt-0">
+                                    Ada beberapa quotation yang tidak perlu location contoh nya pembuatan Kartu member, Tour De, Charge Sound, Makan, Foto Prewed . Mengatasi hal ini location bisa di isi dengan tanda “-“.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item mb-3">
+                            <h2 class="accordion-header " id="panelsStayOpen-headingsix">
+                                <button class="accordion-button collapsed text-black fs-7 text-dark" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapsesix" aria-expanded="false" aria-controls="panelsStayOpen-collapsesix">
+                                    Bagaimana jika customer tidak memberikan informasi detail ?
+                                </button>
+                            </h2>
+                            <div id="panelsStayOpen-collapsesix" class="accordion-collapse collapse " aria-labelledby="panelsStayOpen-headingsix">
+                                <p></p>
+                                <div class="accordion-body fs-7 fw-500 pt-0">
+                                    Jika customer tidak memberikan informasi detail seperti, email atau alamat maka data bisa di isi secara dummy. Tetapi perlu di perhatikan data ini berdampak pada data Customer & Form Quotation
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item mb-3">
+                            <h2 class="accordion-header " id="panelsStayOpen-headingsix">
+                                <button class="accordion-button collapsed text-black fs-7 text-dark" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapsesix" aria-expanded="false" aria-controls="panelsStayOpen-collapsesix">
+                                    Bagaimana jika ada penambahan item pada quotation yang sama?
+                                </button>
+                            </h2>
+                            <div id="panelsStayOpen-collapsesix" class="accordion-collapse collapse " aria-labelledby="panelsStayOpen-headingsix">
+                                <p></p>
+                                <div class="accordion-body fs-7 fw-500 pt-0">
+                                    <!-- lorem -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="text-dark">
+            &copy; 2023 Tim IT DevOps MNR.
+        </footer>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/lightbox/js/lightbox.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
